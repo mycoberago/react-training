79cccb452765fb2cbca811e994a27bf95c4b86d4
Mike Berago <mycoberago@Mycos-MBP.lan>
2017-01-12 22:28:43 -0600

    displaying child components and using spread operator

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,3 +1,7 @@
+import Terrain from 'react-icons/lib/md/terrain'
+import SnowFlake from 'react-icons/lib/ti/weather-snow'
+import Calendar from 'react-icons/lib/fa/calendar'
+
 const percentToDecimal = (decimal) => {
 	return((decimal * 10) + '%')
 }
@@ -6,20 +10,23 @@ const calcGoalProgress = (total,goal) => {
 	return percentToDecimal(total/goal)
 }
 	
-
+//  stateless component
 export const SkiDayCount = ({total, backcountry, powder, goal}) => (
 
 	<div className="ski-day-count">
 		<div className="total-days">
 			<span>{total}</span>
+			<Calendar />
 			<span> Days</span>
 		</div>
 		<div className="powder-days">
 			<span>{powder}</span>
+			<SnowFlake />
 			<span> Days</span>
 		</div>
 		<div className="backcountry-days">
 			<span>{backcountry}</span>
+			<Terrain />
 			<span> Days</span>
 		</div>
 		<div>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { hello, goodbye } from './lib'
-import { SkiDayCount } from './components/SkiDayCount'
+import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 
 render(
-	<SkiDayCount total={50} 
-				 powder={20}
-				 backcountry={10}
-				 goal={10} />,
+	<SkiDayList days={
+		[
+			{
+				resort: "Squaw Valley",
+				date: new Date('1/2/2016'),
+				powder: true,
+				backcountry: false
+			},
+			{
+				resort: "Dells",
+				date: new Date('5/23/2016'),
+				powder: false,
+				backcountry: false
+			},
+			{
+				resort: "Philippines",
+				date: new Date('1/5/2015'),
+				powder: false,
+				backcountry: true
+			}
+		]
+	}/>,
 	document.getElementById('react-container')
 )
